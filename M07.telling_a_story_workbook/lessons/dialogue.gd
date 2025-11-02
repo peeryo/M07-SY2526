caece9bd516ed9aeea85414f23115f50c52bc9a8
[peeryo] <peeryo@sfusd.edu>
2025-11-02 07:29:19 -0800

    M7L10 completed

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -2,20 +2,48 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-var dialogue_items : Array[String] = [
-	"I'm learning about arrays . . .",
-	". . . it's a little complicated",
-	"Let's see if I got it right: an array is a list of values!",
-	"Did I get it right? Did I?",
-	"Hehe! Bye bye~!",
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+	"regular": preload("res://assets/emotion_regular.png")
+}
+
+var dialogue_items : Array[Dictionary] = [
+	{	"expression": expressions["regular"],
+		"text": "I'm learning about arrays . . .",
+	},
+
+	{
+		"expression": expressions["sad"],
+		"text":". . . it's a little complicated",
+	},	
+	
+	{
+		"expression": expressions["happy"],
+		"text": "Let's see if I got it right: an array is a list of values!",
+	},	
+	
+	{
+		"expression": expressions["regular"],
+		"text": "Did I get it right? Did I?",
+	},
+
+	{	
+		"expression": expressions["happy"],
+		"text": "Hehe! Bye bye~!",
+	}
 ]
 
 var current_item_index := 0
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
 	var text_appearing_duration := 1.2
